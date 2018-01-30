e167ff7251052e096449c50427b95fb2777a37d6
高橋篤史 <AtsushiTakahashi@takahashiatsushi-no-MacBook-Pro.local>
2018-01-30 13:52:45 +0900

    make_routing
    ルーティングの設定
    コントローラーにアクションのみ設定
    不要なファイルが作成されないように調整

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,25 @@
 class ArticlesController < ApplicationController
+
   def index
   end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+
 end
