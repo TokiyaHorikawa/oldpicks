98f541261e32b4017f4e36f1f6283a2f704fce9f
高橋篤史 <AtsushiTakahashi@takahashiatsushi-no-MacBook-Pro.local>
2018-02-03 11:26:15 +0900

    sign_up&sign_in
    sign_upとsign_in機能の実装
    ログイン失敗時にはflash吐かせる

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates :name, presence: true
 end
+
