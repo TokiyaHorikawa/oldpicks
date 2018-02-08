66d7d183d3035e50083e56a2a66348782b5e1bb6
高橋篤史 <AtsushiTakahashi@takahashiatsushi-no-MacBook-Pro.local>
2018-02-08 19:23:25 +0900

    edit-user-route
    ユーザー編集、削除のためのルーティングとコントローラーの設定。
    モデルはユーザーが消えたら、ユーザーが投稿した記事・コメントなども一緒に消えるように編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,27 @@
 class UsersController < ApplicationController
 
+before_action :authenticate_user!
+
   def show
   end
 
   def edit
-    @user = User.find(params[:id])
-    @articles = Article.new
-    @comments = Comment.new
+    @user = User.find(current_user)
+    @article = Article.new
+    @comment = Comment.new
   end
 
   def update
-    @user = User.find(params[:id])
+    @user = User.find(current_user)
     @user.update(user_params)
     redirect_to :root
   end
 
+  def destroy
+    user = User.find(current_user)
+    user.destroy
+    redirect_to :root, notice: 'アカウントの削除が完了しました'
+  end
   private
     def user_params
       params.require(:user).permit(
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,22 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :articles
-  has_many :comments
+  mount_uploader :avatar_image, AvatarImageUploader
+
+  has_many :articles, dependent: :destroy
+  has_many :comments, dependent: :destroy
 
   validates :name, presence: true
+
+  def user_profile
+    if company.present? && position.present?
+      return "#{company.truncate(15)}\n#{position.truncate(15)}"
+    elsif company.present?
+      company.truncate(15)
+    elsif position.present?
+      position.truncate(15)
+    end
+  end
 
 end
 
