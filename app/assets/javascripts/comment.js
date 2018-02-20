26514196c31ddf23531a919381f8644df8ef09dc
高橋篤史 <AtsushiTakahashi@takahashiatsushi-no-MacBook-Pro.local>
2018-02-20 16:00:12 +0900

    js-rewrite
    非同期通信した時にuser-iconがなかった場合の処理を追加

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -8,7 +8,7 @@ $(function(){
 
   // フォームのデータ送信時にログインユーザーのコメント表示部分に差し込むhtml
     var html =  '<div class = "section-header">' +
-                  'フォローしているユーザーのコメント' +
+                  'あなたのコメント' +
                 '</div>' +
                 '<div class="comment-container">' +
                   '<div class="comment-row-no-border">' +
@@ -21,10 +21,6 @@ $(function(){
                           comment.name +
                         '</div>' +
                         '<div class="follower-only">' +
-                          '<img class="follower-only-icon" src="/assets/follower-only.png" alt="Follower only">' +
-                          '<div class="follower-only-text">' +
-                            'フォロワーのみ' +
-                          '</div>' +
                         '</div>' +
                         '<div class="user-job">' +
                           comment.company +
--- app/views/comments/create.json.jbuilder
+++ app/views/comments/create.json.jbuilder
@@ -2,7 +2,7 @@ json.id             @comment.id
 json.user_id        @comment.user.id
 json.name           @comment.user.name
 json.content        @comment.content
-json.avatar_image   @comment.user.avatar_image.url
+json.avatar_image   @comment.user.avatar_image.url ? @comment.user.avatar_image.url : "/uploads/no-user-image.png"
 json.company        @comment.user.company
 json.position       @comment.user.position
 json.time           @comment.before_time
