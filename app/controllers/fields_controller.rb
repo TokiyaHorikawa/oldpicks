f2e30ffaf79e797d6298181fde801ad7685d3a41
kataokatakuya <kataokatakuya>
2018-02-21 17:26:34 +0900

    Edit  action and view

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -5,36 +5,42 @@ class FieldsController < ApplicationController
 
   def technology
     @tech_articles = Article.tagged_with("テクノロジー").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("テクノロジー").order('created_at DESC').limit(8)
   end
 
   def business
     @business_articles = Article.tagged_with("ビジネス").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("ビジネス").order('created_at DESC').limit(8)
   end
 
   def economy
     @economy_articles = Article.tagged_with("政治").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("政治").order('created_at DESC').limit(8)
   end
 
   def finance
     @finance_articles = Article.tagged_with("金融").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("金融").order('created_at DESC').limit(8)
   end
 
   def career
     @career_articles = Article.tagged_with("キャリア").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("キャリア").order('created_at DESC').limit(8)
   end
 
   def society
     @society_articles = Article.tagged_with("社会").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("社会").order('created_at DESC').limit(8)
   end
 
   def job
     @job_articles = Article.tagged_with("ジョブ").order('comment_counts DESC').limit(10)
+    @latest_articles = Article.tagged_with("ジョブ").order('created_at DESC').limit(8)
   end
 
   def set_user_article
     @article = Article.new
     @comment = Comment.new
-    @latest_articles = Article.order('created_at DESC').limit(8)
   end
 
   def set_user_follow
