f1035a1b0e3149c26ba271c880b27b4c2a3024a6
TokiyaHorikawa <ayikot4423@gmail.com>
2018-02-27 12:12:03 +0900

    変更（戻す）

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -89,8 +89,7 @@ $(function(){
                               'シェアする' +
                             '</div>' +
                             '<div class="sns-twitter">' +
-                              // '<img class="twitter-mark" src="/assets/twitter-gray.png" alt="Twitter gray">' +
-                              '<img class="twitter-mark" src="/images/twitter-gray.png" alt="Twitter gray">' +
+                              '<img class="twitter-mark" src="/assets/twitter-gray.png" alt="Twitter gray">' +
                             '</div>' +
                             '<div class="sns-facebook">' +
                               '<img class="facebook-mark" src="/assets/facebook-gray.png" alt="Facebook gray">' +
