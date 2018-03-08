ccce9eca98d763bf95dddafb4635a101dc9120fe
TokiyaHorikawa <ayikot4423@gmail.com>
2018-03-08 18:50:02 +0900

    変更（unicorn.rb 自動デプロイ関連）

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -40,3 +40,7 @@ end
 after_fork do |_server, _worker|
   defined?(ActiveRecord::Base) && ActiveRecord::Base.establish_connection
 end
+
+before_exec do |server|
+  ENV["BUNDLE_GEMFILE"] = File.join(File.expand_path("../../../../", __FILE__), "current", "Gemfile")
+end
